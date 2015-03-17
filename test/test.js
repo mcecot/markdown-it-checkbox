b51b98ade52d04e3e2fd2f32c8a74e9f1caeb747
markus cecot <mail@cecot.de>
2015-03-17 13:54:38 +0100

    fixing test.

--- test/test.js
+++ test/test.js
@@ -5,6 +5,6 @@ var generate  = require('markdown-it-testgen');
 /*eslint-env mocha */
 describe("markdown-it-checkbox", function() {
   var md;
-  md = require("markdown-it")().use(require("../build"));
+  md = require("markdown-it")().use(require("../"));
   generate(path.join(__dirname, "fixtures/checkbox.txt"), md);
 });
